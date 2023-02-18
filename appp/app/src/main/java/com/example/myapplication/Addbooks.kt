9d13e5a95774fb051d49cdaaea5260b4ac08bd82
sashadioma <102321331+sashadioma@users.noreply.github.com>
2023-02-18 15:04:50 +0200

    Реалізація додання книжок і читачів

--- appp/app/src/main/java/com/example/myapplication/Addbooks.kt
+++ appp/app/src/main/java/com/example/myapplication/Addbooks.kt
@@ -4,19 +4,40 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.myapplication.databinding.ActivityAddbooksBinding
 import com.example.myapplication.databinding.ActivityMenuBinding
 
 class Addbooks : AppCompatActivity() {
     lateinit var bindingClass : ActivityAddbooksBinding
+    lateinit var db: DB
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityAddbooksBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
+        db = DB.getDB(this)
     }
     fun onClick(view: View){
-        val intent = Intent(this,Menu::class.java)
-        startActivity(intent)
-        finish()
+
+        val book = Book(
+            null, bindingClass.name.text.toString(),
+            bindingClass.autor.text.toString(),
+            bindingClass.year.text.toString(),
+            bindingClass.ryad.text.toString(),
+            bindingClass.stelaz.text.toString(),
+            bindingClass.polycia.text.toString(),0
+        )
+        ATask(db = db,2) { resalt ->
+            if (resalt) {
+                val intent = Intent(this, Menu::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                runOnUiThread{
+                    Toast.makeText(this,"ERROR", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }.execute(book)
     }
 }
--- appp/app/src/main/java/com/example/myapplication/Addreaders.kt
+++ appp/app/src/main/java/com/example/myapplication/Addreaders.kt
@@ -4,19 +4,38 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.myapplication.databinding.ActivityAddbooksBinding
 import com.example.myapplication.databinding.ActivityAddreadersBinding
 
 class Addreaders : AppCompatActivity() {
     lateinit var bindingClass : ActivityAddreadersBinding
+    lateinit var db: DB
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityAddreadersBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
+        db = DB.getDB(this)
     }
     fun onClick(view: View){
-        val intent = Intent(this,Menu::class.java)
-        startActivity(intent)
-        finish()
+        val reader = Reader(
+            null, bindingClass.firstName.text.toString(),
+            bindingClass.lastName.text.toString(),
+            bindingClass.adressa.text.toString(),
+            bindingClass.phone.text.toString().toInt(),0
+        )
+        ATask(db = db,3) { resalt ->
+            if (resalt) {
+                val intent = Intent(this, Menu::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                runOnUiThread{
+                    Toast.makeText(this,"ERROR", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }.execute(reader)
     }
-}
+    }
+
